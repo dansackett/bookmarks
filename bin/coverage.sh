378d68cc6496dd70dc28991fbd9b3ce5b478395e
Dan Sackett <danesackett@gmail.com>
2013-03-23 14:44:29 -0400

    Fix coverage stuff and add __init__ scripts so no conflicts arise with pytest

--- bin/coverage.sh
+++ bin/coverage.sh
@@ -1,11 +1,10 @@
 py.test --cov-config .coveragerc \
         --cov-report html \
         --cov tests \
-        --cov bookshelf \
         --cov bookmarks \
         --cov account \
         --cov auth \
         --cov tags && \
 python -c "import webbrowser, os; \
            webbrowser.open('file://' + os.getcwd() + \
-                           '/.coverage_html/index.html')"
+                           '/htmlcov/index.html')"
