2a8747d8f47cda3a497c4c0bd3ddb50ff7c84bc8
Dan Sackett <danesackett@gmail.com>
2013-03-26 23:43:13 -0400

    Sorted if most_popular tags should be by title as a secondary. This is a patch as it's still in reverse alpha and I'd like it to not be reversed.

--- tags/utils.py
+++ tags/utils.py
@@ -2,8 +2,8 @@ from tags.models import Tag
 
 
 def most_popular(user):
-    tags = Tag.objects.filter(user=user)
-    return sorted([(tag.bookmarks_count(tag), tag) for tag in tags], reverse=True)
+    tags = Tag.objects.filter(user=user).order_by('title')
+    return sorted([(tag.bookmarks_count(tag), tag.title, tag) for tag in tags], reverse=True)
 
 
 def total_tags(user):
