a58cef1c8c929604c714c69b920a0092cb285dae
Dan Sackett <danesackett@gmail.com>
2013-08-16 16:08:06 -0400

    Cleaned up AJAX calls with single function and callback functions for events

--- public/js/global.js
+++ public/js/global.js
@@ -1,62 +1,83 @@
 $(document).ready(function() {
-    // AJAX submission of favoriting bookmark
-    $('.favorite-bookmark').submit(function() {
-        $.ajax({
-            data: $(this).serialize(),
-            type: $(this).attr('method'),
-            url: $(this).attr('action'),
-            success: function(response) {
-                if(response) {
-                    // Gather elements to manipulate
-                    icon = $('.' + response + ' .link-button i');
-                    current_class = icon.attr('class');
+    ///////////////////////////////////////////
+    ///////////////// AJAX ////////////////////
+    ///////////////////////////////////////////
 
-                    // If bookmark is currently starred, change to un-starred
-                    if (current_class == 'star icon-star'){
-                        icon.removeClass('icon-star');
-                        icon.addClass('icon-star-empty');
-                    }
-                    // If bookmark is not currently starred, change to
-                    else {
-                        icon.removeClass('icon-star-empty');
-                        icon.addClass('icon-star');
+    // AJAX submission function
+    function ajax_submit(selector, _function) {
+        $(selector).submit(function() {
+            $.ajax({
+                data: $(this).serialize(),
+                type: $(this).attr('method'),
+                url: $(this).attr('action'),
+                success: function(response) {
+                    if(response) {
+                        _function(response);
                     }
                 }
-            }
+            });
+            return false;
         });
-        return false;
-    });
+    };
 
-    // AJAX submission of completing task
-    $('.complete-task').submit(function() {
-        $.ajax({
-            data: $(this).serialize(),
-            type: $(this).attr('method'),
-            url: $(this).attr('action'),
-            success: function(response) {
-                if(response) {
-                    // Gather elements to manipulate
-                    icon = $('.' + response + ' .link-button i');
-                    current_class = icon.attr('class');
-                    item = $('.item-content.' + response + ' span');
+    // Favorite Bookmark Callback
+    function favorite_bookmark(response) {
+        // Gather elements to manipulate
+        var icon = $('.' + response + ' .link-button i');
+        var current_class = icon.attr('class');
 
-                    // If task is currently completed, change to incomplete
-                    if (current_class == 'star icon-circle'){
-                        icon.removeClass('icon-circle');
-                        icon.addClass('icon-circle-blank');
-                        item.css('text-decoration', 'none');
-                    }
-                    // If task is not currently completely, change
-                    else {
-                        icon.removeClass('icon-circle-blank');
-                        icon.addClass('icon-circle');
-                        item.css('text-decoration', 'line-through');
-                    }
-                }
-            }
+        // If bookmark is currently starred, change to un-starred
+        if (current_class == 'star icon-star'){
+            icon.removeClass('icon-star');
+            icon.addClass('icon-star-empty');
+        }
+        // If bookmark is not currently starred, change to
+        else {
+            icon.removeClass('icon-star-empty');
+            icon.addClass('icon-star');
+        }
+    };
+
+    ajax_submit('.favorite-bookmark', favorite_bookmark);
+
+    // Complete Task Callback
+    function complete_task(response) {
+        var task = $('.' + response + ' .item button');
+
+        if (task.css('text-decoration') == 'line-through') {
+            task.css('text-decoration', 'none');
+        }
+        else {
+            task.css('text-decoration', 'line-through');
+        }
+    };
+
+    ajax_submit('.complete-task', complete_task);
+
+    // Delete Task Callback
+    function delete_task(response) {
+        var task_items = $('.' + response);
+        task_items.each(function() {
+            $(this).hide();
         });
-        return false;
-    });
+    };
+
+    ajax_submit('.delete-task', delete_task);
+
+    // Delete Todolist Callback
+    function delete_todolist(response) {
+        var todolists = $('.' + response);
+        tasks.each(function() {
+            $(this).hide();
+        });
+    };
+
+    ajax_submit('.delete-todolist', delete_todolist);
+
+
+    ///////////////////////////////////////////
+    //////////// External Libraries ///////////
+    ///////////////////////////////////////////
 
     // Use iCheck for checkboxes
     $('input').iCheck({
@@ -90,6 +111,11 @@ $(document).ready(function() {
         var url = $(this).find('a').attr('href');
         window.open(url, "_self");
     });
+
+
+    ///////////////////////////////////////////
+    ////////////// App Functions //////////////
+    ///////////////////////////////////////////
 
     // Show placeholder if there is no value
     $('.field-widget').each(function() {
